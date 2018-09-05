54e332de7a30366d2290803ca72ab9b6452603e9
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-09-05 16:19:41 -0700

    init

--- main.tf
+++ main.tf
@@ -8,12 +8,11 @@ module "bootstrap-nsg" {
     azurerm = "azurerm"
   }
 
-  dcos_role                 = "bootstrap"
-  location                  = "${var.location}"
-  resource_group_name       = "${var.resource_group_name}"
-  tags                      = "${var.tags}"
-  name_prefix               = "${var.name_prefix}"
-  network_security_group_id = "${var.network_security_group_id}"
+  dcos_role           = "bootstrap"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group_name}"
+  tags                = "${var.tags}"
+  name_prefix         = "${var.name_prefix}"
 }
 
 module "dcos-bootstrap-instances" {
