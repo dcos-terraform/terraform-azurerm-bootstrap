0da17262f4ab03adb5cade3f0516feae00bf1bf3
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-09-27 14:20:35 -0700

    terraform fmt after description add

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@
 # Number of Instance
 variable "num_bootstrap" {
   description = "num bootstrap"
-  default = 1
+  default     = 1
 }
 
 # Location (region)
@@ -35,6 +35,7 @@ variable "dcos_instance_os" {
 # Private SSH Key Filename Optional
 variable "ssh_private_key_filename" {
   description = "Path to the SSH private key"
+
   # cannot leave this empty as the file() interpolation will fail later on for the private_key local variable
   # https://github.com/hashicorp/terraform/issues/15605
   default = "/dev/null"
@@ -48,7 +49,7 @@ variable "image" {
 # Disk Type to Leverage. The managed disk type. (optional)
 variable "disk_type" {
   description = "Disk Type to Leverage."
-  default = "Standard_LRS"
+  default     = "Standard_LRS"
 }
 
 # Disk Size in GB
@@ -64,7 +65,7 @@ variable "resource_group_name" {
 # Customer Provided Userdata
 variable "user_data" {
   description = "User data to be used on these instances (cloud-init)"
-  default = ""
+  default     = ""
 }
 
 # SSH User
@@ -80,20 +81,20 @@ variable "public_ssh_key" {
 # Allow stopping for update (bool)
 variable "allow_stopping_for_update" {
   description = "If true, allows Terraform to stop the instance to update its properties"
-  default = "true"
+  default     = "true"
 }
 
 # Add special tags to the resources created by this module
 variable "tags" {
   description = "Add custom tags to all resources"
-  type    = "map"
-  default = {}
+  type        = "map"
+  default     = {}
 }
 
 # Format the hostname inputs are index+1, region, name_prefix
 variable "hostname_format" {
   description = "Format the hostname inputs are index+1, region, cluster_name"
-  default = "bootstrap-%[1]d-%[2]s"
+  default     = "bootstrap-%[1]d-%[2]s"
 }
 
 # Subnet ID
