1cef10986c13e4b4fb94875c76742d4287cd47ba
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-04-04 17:37:00 +0200

    fix: version contraints from modules raised to 0.2.0

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ provider "azurerm" {}
 
 module "dcos-bootstrap-instances" {
   source  = "dcos-terraform/instance/azurerm"
-  version = "~> 0.1.0"
+  version = "~> 0.2.0"
 
   providers = {
     azurerm = "azurerm"
