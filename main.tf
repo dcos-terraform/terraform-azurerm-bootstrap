fb7554b1c979a7b6468b898ab36d43e438c7db94
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-06-24 15:29:31 +0200

    chore: README.md update

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 /**
- * [![Build Status](https://jenkins-terraform.mesosphere.com/service/dcos-terraform-jenkins/job/dcos-terraform/job/terraform-azurerm-bootstrap/job/master/badge/icon)](https://jenkins-terraform.mesosphere.com/service/dcos-terraform-jenkins/job/dcos-terraform/job/terraform-azurerm-bootstrap/job/master/)
+ * [![Build Status](https://jenkins-terraform.mesosphere.com/service/dcos-terraform-jenkins/buildStatus/icon?job=dcos-terraform%2Fterraform-azurerm-bootstrap%2Fsupport%252F0.1.x)](https://jenkins-terraform.mesosphere.com/service/dcos-terraform-jenkins/job/dcos-terraform/job/terraform-azurerm-bootstrap/job/support%252F0.1.x/)
+ *
  * Azure DC/OS Bootstrap Instance
  * ==============================
  * The module creates AzureRM virtual machine instances
