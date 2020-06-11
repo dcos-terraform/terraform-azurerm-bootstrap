44c0648324b326dacabdfa8d4a12b7ef6220af61
Jan Ulferts <julferts@mesosphere.com>
2020-06-11 15:41:41 +0200

    feat: upgrade to tf 0.12

--- main.tf
+++ main.tf
@@ -21,33 +21,34 @@
  ```
  */
 
-provider "azurerm" {}
+provider "azurerm" {
+}
 
 module "dcos-bootstrap-instances" {
   source  = "dcos-terraform/instance/azurerm"
-  version = "~> 0.2.0"
+  version = "~> 0.3.0"
 
   providers = {
-    azurerm = "azurerm"
+    azurerm = azurerm
   }
 
-  cluster_name                      = "${var.cluster_name}"
-  name_prefix                       = "${var.name_prefix}"
-  num                               = "${var.num_bootstrap}"
-  location                          = "${var.location}"
-  vm_size                           = "${var.vm_size}"
-  dcos_instance_os                  = "${var.dcos_instance_os}"
-  ssh_private_key_filename          = "${var.ssh_private_key_filename}"
-  image                             = "${var.image}"
-  disk_type                         = "${var.disk_type}"
-  disk_size                         = "${var.disk_size}"
-  resource_group_name               = "${var.resource_group_name}"
-  network_security_group_id         = "${var.network_security_group_id}"
-  custom_data                       = "${var.custom_data == "" && length(var.image) == 0 ? file("${path.module}/cloudinit.tpl") : var.custom_data}"
-  admin_username                    = "${var.admin_username}"
-  public_ssh_key                    = "${var.public_ssh_key}"
-  tags                              = "${var.tags}"
-  hostname_format                   = "${var.hostname_format}"
-  subnet_id                         = "${var.subnet_id}"
-  avset_platform_fault_domain_count = "${var.avset_platform_fault_domain_count}"
+  cluster_name                      = var.cluster_name
+  name_prefix                       = var.name_prefix
+  num                               = var.num_bootstrap
+  location                          = var.location
+  vm_size                           = var.vm_size
+  dcos_instance_os                  = var.dcos_instance_os
+  ssh_private_key_filename          = var.ssh_private_key_filename
+  image                             = var.image
+  disk_type                         = var.disk_type
+  disk_size                         = var.disk_size
+  resource_group_name               = var.resource_group_name
+  network_security_group_id         = var.network_security_group_id
+  custom_data                       = var.custom_data == "" && length(var.image) == 0 ? file("${path.module}/cloudinit.tpl") : var.custom_data
+  admin_username                    = var.admin_username
+  public_ssh_key                    = var.public_ssh_key
+  tags                              = var.tags
+  hostname_format                   = var.hostname_format
+  subnet_id                         = var.subnet_id
+  avset_platform_fault_domain_count = var.avset_platform_fault_domain_count
 }
